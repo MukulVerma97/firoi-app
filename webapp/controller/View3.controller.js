db63b2e2c2a74347d036cfd5235919ace9f95fb5
Mukul Verma <mukulverma9731@gmail.com>
2025-01-27 12:56:30 +0530

    view3 data pass

--- webapp/controller/View3.controller.js
+++ webapp/controller/View3.controller.js
@@ -4,7 +4,23 @@ sap.ui.define(
     "use strict";
 
     return BaseController.extend("com.infy.aim.controller.View3", {
-      onInit: function () {},
+      onInit: function () {
+this.oRouter = this.getOwnerComponent().getRouter();
+this.oRouter.getRoute("ironman").attachMatched(this.herculis,this);
+
+       },
+
+
+       herculis:function(oEvent){
+       debugger;
+        var superId = oEvent.getParameter("arguments").superheroes;
+        console.log(superId);
+        var sPath = "/superheroes/" + superId;
+        console.log(sPath);
+        this.getView().bindElement(sPath);
+     
+
+       },
       onBack: function () {
         var oHistory, sPreviousHash;
 
